93d3bbe65137b21979919429466eb0f4292aff6c
evertonbrunosds <evertonbrunosds@gmail.com>
2023-11-26 00:18:11 -0300

    v2.3 - Estilização de Categorias

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_task, only: %i[ edit update destroy ]
 
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all.sort_by { |task| task.percentage_days_remaining }
-  end
-
-  # GET /tasks/1 or /tasks/1.json
-  def show
   end
 
   # GET /tasks/new

97ec0d42e49213a098250da73d582c288810faeb
evertonbrunosds <evertonbrunosds@gmail.com>
2023-11-26 00:49:43 -0300

    v2.5 - Criação de tarefa associada a Categoria

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -61,6 +61,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :description, :due_date, :completion, :priority)
+      params.require(:task).permit(:title, :description, :due_date, :completion, :priority, :category_id)
     end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
     has_many :tasks
     validates :name, presence: true
     validates :description, presence: true
+
+    scope :sorted, -> { order(:name) }
+
 end

5b911741e9b7720c206baaa20b52ed46a91b6a5d
evertonbrunosds <evertonbrunosds@gmail.com>
2023-11-27 23:32:41 -0300

    v3.3 - exibe quantidade de tarefas por categoria

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+
+    def count_completed_tasks_in_category(category)
+        return category.tasks.where(completion: true).count
+    end
+    
+    def count_incomplete_tasks_in_category(category)
+        return category.tasks.where(completion: false).count
+    end
+
 end

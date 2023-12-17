6e8c2e1f91caaa99ddeb22a227312b4743023ad3
evertonbrunosds <evertonbrunosds@gmail.com>
2023-12-17 00:03:55 -0300

    v4.0 - povoamento usando seed

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -13,13 +13,13 @@ module TasksHelper
         return 'bg-secondary'
       end
       percentage = task.percentage_days_remaining.round(2)
-      if  0 < percentage && percentage <= 33.33
+      if  percentage <= 33.33
         return 'bg-danger'
       end
       if  33.33 < percentage && percentage <= 66.66
         return 'bg-warning'
       end
-      if  66.66 < percentage && percentage <= 100
+      if  66.66 < percentage
         return 'bg-success'
       end
       return ''
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,45 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+p 'Iniciando ...'
+
+def text_generator
+    words = []
+    500.times do
+      length_word = rand(2..10)
+      word = ""
+      length_word.times do
+        word << rand(97..122).chr
+      end
+      words << word
+    end
+    text = words.join(' ')
+    return text
+end
+
+Task.delete_all
+
+Category.delete_all
+
+User.all.each do |user|
+    15.times do
+        Category.create!(
+            name: "Category #{rand(10000)}",
+            description: text_generator(),
+            user_id: user.id
+        )
+    end
+end
+
+Category.all.each do |category|
+    100.times do
+        Task.create!(
+            title: "Task #{rand(10000)}",
+            description: text_generator(),
+            due_date:  Date.today + rand(5..365),
+            completion: rand(2) == 1,
+            priority: ['low', 'medium', 'high'].sample,
+            created_at: Date.today - rand(5..365),
+            category_id: category.id
+        )
+    end
+end
+
+p 'Terminou!'
